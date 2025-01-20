a438185b5915c571fa8cbd0dd8bf28da601dca67
wasteapp111@gmail.com <Diordi1>
2025-01-21 01:00:58 +0530

    mob-update-0.1.3

--- src/Main/CourseTable.jsx
+++ src/Main/CourseTable.jsx
@@ -1,19 +1,19 @@
 function CourseTable() {
     return (  
-        <div className="sm:w-[48%] w-[100%]">
+        <div className="sm:w-[48%] w-[100%] border border-cyan-300">
         <div><p className="font-semibold text-xl mb-4">Weekly Sale Stats</p></div>
         <div class="relative overflow-x-auto  sm:rounded-lg w-fit">
             <table class="sm:w-[48%] w-[100%] text-xs sm:text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-4 sm:px-6 py-3 text-gray-400 font-semibold ">
+                        <th scope="col" class="px-2 sm:px-6 py-3 text-gray-400 font-semibold ">
                            
                             Course
                         </th>
-                        <th scope="col" class="px-4 sm:px-6 py-3 text-gray-400 font-semibold">
+                        <th scope="col" class="px-2 sm:px-6 py-3 text-gray-400 font-semibold">
                             Sale
                         </th>
-                        <th scope="col" class="px-4 sm:px-6 py-3 text-gray-400 font-semibold">
+                        <th scope="col" class="px-2 sm:px-6 py-3 text-gray-400 font-semibold">
                             Earnings
                         </th>
                         
@@ -21,7 +21,7 @@ function CourseTable() {
                 </thead>
                 <tbody>
                     <tr class="bg-white  dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <th scope="row" class="px-2 sm:px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         <div className="flex sm:gap-4 gap-2">
                                 <div className="pic p-3 rounded bg-stone-700 text-white h-12 text-xl font-bold w-12 text-center">P</div>
                                 <div className="content flex flex-wrap leading-4"> Apple MacBook Pro</div>
@@ -38,7 +38,7 @@ function CourseTable() {
                     </tr>
                    
                     <tr class="bg-white  dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <th scope="row" class="px-2 sm:px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         <div className="flex gap-4  ">
                                 <div className="pic p-3 rounded bg-green-400 text-white h-12 text-xl font-bold w-12 text-center"><img src="book.png"></img></div>
                                 <div className="content flex flex-wrap leading-4"> Book Purchase</div>
